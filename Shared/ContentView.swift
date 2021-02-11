f6817895e6621d9a9295a444e5f68a4362bd92d4
Roderic Campbell <roderic@gmail.com>
2021-02-10 20:32:31 -0800

    Implement the nested list using an OutlineGroup

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,11 +7,46 @@
 
 import SwiftUI
 
+public struct ItemWithChildren: Decodable, Identifiable, Equatable {
+    public let id: UUID
+    public let title: String
+    public let children: [ItemWithChildren]?
+    
+    static let tree: [ItemWithChildren] = [
+        .init(id: UUID(), title: "a",
+              children: [
+                .init(id: UUID(), title: "b", children: nil),
+                .init(id: UUID(), title: "c", children: nil)
+              ]),
+        .init(id: UUID(), title: "b", children: nil),
+        .init(id: UUID(), title: "c", children: nil)
+    ]
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            OutlineGroup (ItemWithChildren.tree, children: \.children) { item in
+                Text(item.title)
+                    .onDrag { NSItemProvider(object: item.id.uuidString as NSString) }
+                    .onDrop(
+                        of: ["public.text"],
+                        delegate: StringDropDelegate()
+                    )
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
     }
+}
+
+struct StringDropDelegate: DropDelegate {
+    func performDrop(info: DropInfo) -> Bool {
+        print(info)
+        return true
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
