50d32a40d46dc0082b33bb995f73cdad274dcaad
Roderic Campbell <roderic@gmail.com>
2021-02-10 21:13:45 -0800

    Remove the Outline group to keep it simple

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
+import MobileCoreServices
 
 public struct ItemWithChildren: Decodable, Identifiable, Equatable {
     public let id: UUID
@@ -13,40 +15,30 @@ public struct ItemWithChildren: Decodable, Identifiable, Equatable {
     public let children: [ItemWithChildren]?
     
     static let tree: [ItemWithChildren] = [
-        .init(id: UUID(), title: "a",
+        .init(id: UUID(), title: "Some Top Level Thing",
               children: [
-                .init(id: UUID(), title: "b", children: nil),
-                .init(id: UUID(), title: "c", children: nil)
+                .init(id: UUID(), title: "badfadf", children: nil),
+                .init(id: UUID(), title: "cdasfadsf", children: nil)
               ]),
-        .init(id: UUID(), title: "b", children: nil),
-        .init(id: UUID(), title: "c", children: nil)
+        .init(id: UUID(), title: "Another top level thing", children: nil),
+        .init(id: UUID(), title: "More Things", children: nil)
     ]
     
 }
 
 struct ContentView: View {
     var body: some View {
-        List {
-            OutlineGroup (ItemWithChildren.tree, children: \.children) { item in
-                Text(item.title)
-                    .onDrag { NSItemProvider(object: item.id.uuidString as NSString) }
-                    .onDrop(
-                        of: ["public.text"],
-                        delegate: StringDropDelegate()
-                    )
-            }
+        List (ItemWithChildren.tree) { item in
+            Label(item.title, systemImage: "rectangle.3.offgrid.fill")
+                .padding()
+                .background(Color.secondary)
+                .onDrag { NSItemProvider(object: item.id.uuidString as NSString) }
+                .onDrop(of: [.text], isTargeted: nil, perform: { providers -> Bool in
+                    print(providers)
+                    return true
+                })
         }
-        .listStyle(InsetGroupedListStyle())
     }
-}
-
-struct StringDropDelegate: DropDelegate {
-    func performDrop(info: DropInfo) -> Bool {
-        print(info)
-        return true
-    }
-    
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
